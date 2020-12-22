f9e238a5be0bf940c718d6b464773378cd876a89
Deividas Gelžinis <limessi61@gmail.com>
2020-12-22 22:59:52 +0200

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -43,7 +43,7 @@ export default function Login() {
                             <Form.Control type="email" ref={emailRef} required />
                         </Form.Group>
                         <Form.Group id = "password">
-                            <Form.Label>password</Form.Label>
+                            <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group> 
                         <Button disabled = {loading} className ="w-100"type="submit">Log In</Button>
